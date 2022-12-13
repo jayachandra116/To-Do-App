7d7d082b9edb5b3d1d67a6fa98976689b336fd63
Jaya Chandra <mjchandra97@gmail.com>
2022-12-13 20:53:18 +0530

    Added a sample list of todo items

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,15 @@
-
 function App() {
+  let toDos = ["Learn React", "Learn NodeJS", "Learn Express", "Learn MongoDB"];
+
   return (
+    <div>
+      <h1>To-Do App</h1>
+      <ul>
+        {toDos.map((toDo) => (
+          <li key={toDo}>{toDo}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
